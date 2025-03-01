5432de933ece7195cefe6b77407c2e672ba465e8
CWX <cwxdev@outlook.com>
2025-03-01 21:09:10 +0000

    add check for existing webview, change titlebar to be the same colour as the navmenu, fix settings menu options

--- CWX-SPT-Frontend/App.xaml.cs
+++ CWX-SPT-Frontend/App.xaml.cs
@@ -1,10 +1,41 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using Microsoft.Web.WebView2.Core;
 
 namespace CWX_SPT_Frontend;
 
-/// <summary>
-///     Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        CheckForWebview();
+        base.OnStartup(e);
+    }
+
+    /// <summary>
+    /// Yoinked from AcidPhantasm https://github.com/acidphantasm/acidphantasm-apbs/blob/main/ConfigApp/App.xaml.cs
+    /// </summary>
+    private void CheckForWebview()
+    {
+        try
+        {
+            CoreWebView2Environment.GetAvailableBrowserVersionString();
+        }
+        catch (Exception e)
+        {
+            var messageBoxTitle = $"Missing Webview2 Runtime";
+            var messageBoxMessage = $"You are missing the Evergreen Bootstrapper, which is required to use WebView2 applications. \n\n Please download and install the Evergreen Bootstrapper or Standalone Installer (if offline). \n\n https://developer.microsoft.com/en-us/microsoft-edge/webview2";
+            var messageBoxButtons = MessageBoxButton.OKCancel;
+
+            // Let the user decide if the app should die or not (if applicable).
+            if (MessageBox.Show(messageBoxMessage, messageBoxTitle, messageBoxButtons) == MessageBoxResult.OK)
+            {
+                Process.Start(new ProcessStartInfo { FileName = "https://developer.microsoft.com/en-us/microsoft-edge/webview2", UseShellExecute = true });
+            }
+            else
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
 }
--- CWX-SPT-Frontend/MainWindow.xaml.cs
+++ CWX-SPT-Frontend/MainWindow.xaml.cs
@@ -1,22 +1,25 @@
 ﻿using System.ComponentModel;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Threading;
 using CWX_SPT_Launcher_Backend.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 using MudBlazor;
 using MudBlazor.Services;
+using MediaColor = System.Windows.Media.Color;
 using Color = System.Drawing.Color;
+using Colors = MudBlazor.Colors;
 
 namespace CWX_SPT_Frontend;
 
-/// <summary>
-///     Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow
 {
     public static Window WindowMain;
     private readonly SettingsHelper _settings;
+    private readonly MediaColor _titleBarColor = MediaColor.FromArgb(255, 39, 39, 47);
 
     public MainWindow()
     {
@@ -45,22 +48,39 @@ public partial class MainWindow
 
         InitializeComponent();
         CustomizeComponent();
-
-        var hWnd = new WindowInteropHelper(this).EnsureHandle();
-        var value = true;
-        DwmSetWindowAttribute(
-            hWnd,
-            DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE,
-            ref value,
-            Marshal.SizeOf<bool>());
-
+        CustomizeWindow();
         WindowMain = this;
+    }
+
+    private void CustomizeWindow()
+    {
+        // Defer the DWM call - this is to stop issues happening later regarding to changes to the Window.
+        Dispatcher.BeginInvoke(new Action(() =>
+        {
+            var hWnd = new WindowInteropHelper(this).EnsureHandle();
+            // This is the NavBar Background colour.
+            int colorRef = _titleBarColor.R | (_titleBarColor.G << 8) | (_titleBarColor.B << 16);
+
+            DwmSetWindowAttribute(
+                hWnd,
+                DWMWINDOWATTRIBUTE.DWMWA_CAPTION_COLOR,
+                ref colorRef,
+                Marshal.SizeOf<bool>()
+            );
+        }), DispatcherPriority.ContextIdle);
+
+        Dispatcher.BeginInvoke(new Action(async () =>
+        {
+            // Webview and WPF should be init by this point, *hopefully* removing all white flashes
+            WindowMain.WindowState = WindowState.Normal;
+        }), DispatcherPriority.ContextIdle);
     }
 
     private void CustomizeComponent()
     {
         BlazorWebViewControl.BlazorWebViewInitialized += (sender, args) =>
         {
+            // Do this to mitigate the white flash from webview
             args.WebView.DefaultBackgroundColor = Color.FromArgb(255, 50, 51, 61);
         };
 
@@ -89,18 +109,22 @@ public partial class MainWindow
         _settings.SetClientLocationSettings((int) Top, (int) Left);
         _settings.SetClientSizeSettings((int) Height, (int) Width);
         _settings.SetFirstRun(false);
+
+        if (_settings.GetSettings().AppSettings.CloseToTray)
+        {
+            WindowMain.WindowState = WindowState.Minimized;
+            e.Cancel = true;
+            return;
+        }
+
         base.OnClosing(e);
     }
 
     [DllImport("Dwmapi.dll")]
-    private static extern int DwmSetWindowAttribute(
-        IntPtr hwnd,
-        DWMWINDOWATTRIBUTE attribute,
-        [In] ref bool pvAttribute,
-        int cbAttribute);
+    private static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE attribute, [In] ref int pvAttribute, int cbAttribute);
 
     private enum DWMWINDOWATTRIBUTE
     {
-        DWMWA_USE_IMMERSIVE_DARK_MODE = 20
+        DWMWA_CAPTION_COLOR = 35
     }
 }
