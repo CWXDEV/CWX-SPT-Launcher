5e29e16c9e15c8f18b12374d85887bc3ece913bc
CWX <cwxdev@outlook.com>
2025-03-27 22:20:06 +0000

    implement dummy unloaded/loaded/order drag-drop

--- Spt.Frontend/MainWindow.xaml.cs
+++ Spt.Frontend/MainWindow.xaml.cs
@@ -19,7 +19,6 @@ public partial class MainWindow
 {
     public static Window WindowMain;
     private readonly ConfigHelper _config;
-    private readonly HttpHelper _httpHelper;
     private readonly MediaColor _titleBarColor = MediaColor.FromArgb(255, 39, 39, 47);
 
     public MainWindow()
@@ -49,8 +48,13 @@ public partial class MainWindow
         Resources.Add("services", serviceProvider);
 
         _config = serviceProvider.GetRequiredService<ConfigHelper>();
-        _httpHelper = serviceProvider.GetRequiredService<HttpHelper>();
-        _httpHelper.IsInternetAccessAvailable();
+
+        var httpHelper = serviceProvider.GetRequiredService<HttpHelper>();
+        httpHelper.IsInternetAccessAvailable();
+
+        var modLoader = serviceProvider.GetRequiredService<ModHelper>();
+        _ = modLoader.GetServerMods();
+        _ = modLoader.GetClientMods();
 
         InitializeComponent();
         CustomizeComponent();
