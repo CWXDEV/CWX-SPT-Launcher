7b6a1771cc3a0b2a7bad36fbb2f012e46b33eba4
CWX <cwxdev@outlook.com>
2025-05-18 17:40:01 +0100

    add external link loading

--- Spt.Frontend/Resources/app.js
+++ Spt.Frontend/Resources/app.js
@@ -22,3 +22,15 @@ window.onscroll = function() {
 };
 
 window.toggleScrollButton();
+
+// Cuz Photino starts links to external pages inside the app
+// add this script to check a tag has _blank and start it externally ourself
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.addEventListener('click', function (e) {
+        const target = e.target.closest('a');
+        if (target && target.href && target.target === '_blank' && !target.href.startsWith('#')) {
+            e.preventDefault();
+            window.external.sendMessage('open-external:' + target.href);
+        }
+    });
+});
