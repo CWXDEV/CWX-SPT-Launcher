95727114e3e1074efedc3edd900419ecb2609b8b
CWX <cwxdev@outlook.com>
2025-09-06 00:33:32 +0100

    Fix Server defaults

--- Spt.Core/Models/App/Server.cs
+++ Spt.Core/Models/App/Server.cs
@@ -2,8 +2,8 @@
 
 public class Server
 {
-    public string Name { get; set; } = "127.0.0.1:6969";
-    public string IpAddress { get; set; } = "LocalHost";
+    public string Name { get; set; } = "LocalHost";
+    public string IpAddress { get; set; } = "127.0.0.1:6969";
     public string ServerId { get; set; } = "1721162719";
     public bool Locked { get; set; } = true;
 }
